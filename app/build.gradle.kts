54fdfc61d9161af822985cf87d8d31805b965d1c
Jure Merhar <dev@merhar.si>
2026-03-02 11:49:13 +0100

    chore: release v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "si.merhar.sweetspot"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
